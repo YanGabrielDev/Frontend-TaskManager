287acbe842788cac05a55a7ed292dfde63ef3d93
Yan_Gabriel <yangabriel2012@gmail.com>
2024-06-17 22:26:38 -0400

    refatorando page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,28 +3,16 @@ import { Grid } from '@/components/Grid'
 import wallpaper from '../../../public/wallpaper.png'
 import Image from 'next/image'
 import { Form } from './components/Form'
+import * as stylex from 'styled-jsx/style'
+import { ImageLogin } from './components/ImageLogin'
 export default function Login() {
   return (
     <Grid container>
-      <Grid xlarge={8} large={8} medium={6}>
-        <Image
-          src={wallpaper}
-          alt="Pessoas em um fundo branco organizando um quadro de tarefas"
-          objectFit="cover"
-          width={1980}
-          height={1080}
-          style={{
-            height: '100vh',
-            width: '100%',
-            position: 'absolute',
-            objectFit: 'cover',
-          }}
-        />
-      </Grid>
-      {/* )} */}
-
       <Grid xlarge={4} large={4} medium={6} small={12}>
         <Form />
+      </Grid>
+      <Grid xlarge={8} large={8} medium={6}>
+        <ImageLogin/>
       </Grid>
     </Grid>
   )
