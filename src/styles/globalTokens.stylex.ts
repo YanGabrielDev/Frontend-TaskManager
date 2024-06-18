3a242c18f26d9081ced077e2ea82c8e9b6ad8831
Yan_Gabriel <yangabriel2012@gmail.com>
2024-06-17 22:27:38 -0400

    criando tokens

--- src/styles/globalTokens.stylex.ts
+++ src/styles/globalTokens.stylex.ts
@@ -3,3 +3,11 @@ import * as stylex from '@stylexjs/stylex'
 export const fonts = stylex.defineVars({
   roboto: 'Roboto',
 })
+
+export const colors = stylex.defineVars({
+  primary: '#925FE2',
+  secondary: '#9C6FE4',
+  black: '#333437',
+  white: 'FFFFFF',
+  background: "#1C1D21"
+})
